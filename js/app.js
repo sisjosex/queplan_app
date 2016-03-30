2b5e079fc3b3f134b6567e3a507345946cb2cc8a
Jose Luis <sisjosex@gmail.com>
2016-03-29 20:41:52 -0400

    planes_fixes

--- js/app.js
+++ js/app.js
@@ -1756,8 +1756,8 @@ module.controller('NavigatorController', function ($scope) {
 
         loadinitialParams(function(){
 
-            getValidarDeviceUuid(device ? device.uuid : '', '', true);
-            //getValidarDeviceUuid('', '', true);
+            //getValidarDeviceUuid(device ? device.uuid : '', '', true);
+            getValidarDeviceUuid('', '', true);
         });
 
     })
@@ -2016,7 +2016,7 @@ module.controller('PlanesController', function ($scope) {
 
         loadIntoTemplate($(mainnavigator.getCurrentPage().element[0]).find('#planes_content')[0], current_list.items, 'planes_list');
 
-        $(mainnavigator.getCurrentPage().element[0]).find('.list-item-container').height((window.innerHeight - (51 * factor)) / 3);
+        $(mainnavigator.getCurrentPage().element[0]).find('.list-item-container').height((window.innerHeight - (51 * factor)) / 2);
 
         $(mainnavigator.getCurrentPage().element[0]).find('.list-item-container').on('click', function(){
             gotoPlanDetalle( $(this).attr('rel'), current_list );
